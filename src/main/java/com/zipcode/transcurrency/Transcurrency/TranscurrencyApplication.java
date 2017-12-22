9284222bce3301faa886bd693cc63b1a559b1378
Brian R Mullin <brianrmullin@Brians-MacBook-Pro.local>
2017-12-22 11:58:04 -0500

    added file

---
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/TranscurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.zipcode.transcurrency.Transcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscurrencyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zipcode/transcurrency/Transcurrency/TranscurrencyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zipcode.transcurrency.Transcurrency;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TranscurrencyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
