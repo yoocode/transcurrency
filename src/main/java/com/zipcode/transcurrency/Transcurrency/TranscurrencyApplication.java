bd4bd649a117fc294dcb673550a4d20f8578ddb6
Byung-chun Yoo <yoobc1@gmail.com>
2017-12-26 15:32:56 -0500

    Transferred successfully all the codes for OAuth functionality with Facebook login from my demo project

--- src/main/java/com/zipcode/transcurrency/Transcurrency/TranscurrencyApplication.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/TranscurrencyApplication.java
@@ -2,9 +2,38 @@ package com.zipcode.transcurrency.Transcurrency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class TranscurrencyApplication {
+@EnableOAuth2Sso
+@RestController
+public class TranscurrencyApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll()
+				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TranscurrencyApplication.class, args);
