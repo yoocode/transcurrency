c7b71c3095b1950a5cdd9a652c8de10643447ac2
Brian R Mullin <brmullin@email.msmary.edu>
2017-12-27 13:19:35 -0500

    added transfer methods

--- src/main/java/com/zipcode/transcurrency/Transcurrency/Transfer.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/Transfer.java
@@ -1,9 +1,11 @@
 package com.zipcode.transcurrency.Transcurrency;
 
+import java.math.BigDecimal;
+
 public class Transfer {
 
-    
-
+    private bankAccount //field for instantiating bank account object
+    private BigDecimal amount
 
     public void withdraw(bankAccount, amount){
 
