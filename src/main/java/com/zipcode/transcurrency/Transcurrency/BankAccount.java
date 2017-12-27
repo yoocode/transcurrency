e0a3695128a558bdc9a718ce2713f22135d9fca4
Brian R Mullin <brmullin@email.msmary.edu>
2017-12-27 10:42:30 -0500

    changed the author email for git

--- src/main/java/com/zipcode/transcurrency/Transcurrency/BankAccount.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/BankAccount.java
@@ -1,4 +1,11 @@
 package com.zipcode.transcurrency.Transcurrency;
 
 public class BankAccount {
+
+    private long id;
+    private int accountNumber;
+    private int routingNumber;
+    private String bankName;
+
+    
 }
