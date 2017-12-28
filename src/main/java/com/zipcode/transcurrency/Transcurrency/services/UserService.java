2b741e4d9701c6758b02ad089dc53a3c5bff74a6
Byung-chun Yoo <yoobc1@gmail.com>
2017-12-28 15:57:21 -0500

    Added UserService with getAllUsers, addUser, deleteUser methods

---
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/services/UserService.java
@@ -0,0 +1,33 @@
+package com.zipcode.transcurrency.Transcurrency.services;
+
+import com.zipcode.transcurrency.Transcurrency.models.User;
+import com.zipcode.transcurrency.Transcurrency.repositories.IUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers(){
+        List<User> users = new ArrayList<>();
+        userRepository.findAll()
+                .forEach(users::add);
+        return users;
+    }
+
+    public void addUser(User user){
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user){
+        userRepository.delete(user);
+    }
+}
