93a9db4c9d4078540586b4ca81d3a6797fe0cbfa
Brian R Mullin <brmullin@email.msmary.edu>
2017-12-31 13:34:07 -0500

    added BankAccount model

---
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/models/BankAccount.java
@@ -0,0 +1,60 @@
+package com.zipcode.transcurrency.Transcurrency.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BankAccount {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private int accountNumber;
+    private int routingNumber;
+    private String bankName;
+
+    public BankAccount(){
+
+    }
+
+    public BankAccount(Long id, int accountNumber, int routingNumber, String bankName){
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.routingNumber = routingNumber;
+        this.bankName = bankName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(int routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+}
--- src/main/java/com/zipcode/transcurrency/Transcurrency/repositories/BankAccountRepository.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/repositories/BankAccountRepository.java
@@ -1,6 +1,6 @@
 package com.zipcode.transcurrency.Transcurrency.repositories;
 
-import com.zipcode.transcurrency.Transcurrency.controllers.BankAccount;
+import com.zipcode.transcurrency.Transcurrency.models.BankAccount;
 
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 
