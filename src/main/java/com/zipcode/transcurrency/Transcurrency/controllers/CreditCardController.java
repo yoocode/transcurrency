b6eb746d447ab8a2b3a2d7c1952592fa4a54e5d5
Brian R Mullin <brmullin@email.msmary.edu>
2018-01-05 22:01:43 -0500

    added controller REST methods

--- src/main/java/com/zipcode/transcurrency/Transcurrency/controllers/CreditCardController.java
+++ src/main/java/com/zipcode/transcurrency/Transcurrency/controllers/CreditCardController.java
@@ -1,15 +1,26 @@
 package com.zipcode.transcurrency.Transcurrency.controllers;
 
 
+import com.zipcode.transcurrency.Transcurrency.models.CreditCard;
 import com.zipcode.transcurrency.Transcurrency.repositories.CreditCardRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.inject.Inject;
+
 
 @RestController
 public class CreditCardController {
 
     @Inject
     private CreditCardRepository creditCardRepository;
+
+    @RequestMapping(value = "/creditCards", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<CreditCard>> getAllCreditCards() {
+        Iterable<CreditCard> allCreditCards = creditCardRepository.findAll();
+        return new ResponseEntity<>(allCreditCards, HttpStatus.OK);
+    }
 
 }
